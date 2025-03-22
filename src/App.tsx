009a614d60600abfbf738ffb5277034dcb0614d9
Kaspi-Chan <bozhilov53@gmail.com>
2025-03-22 19:42:08 +0200

    Init solid + ts project and add daisyUI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createSignal, type Component } from 'solid-js';
+import styles from './App.module.css';
+
+const App: Component = () => {
+
+  return (
+    <div >
+      App init
+    </div>
+  );
+};
+
+export default App;
